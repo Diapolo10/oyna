38df74344eb82cfcf1996cb0776aecd7801f80a5
kamyarmg <kmgurmia@gmail.com>
2025-01-02 14:56:04 +0330

    fix(explosion): update the game move strategy

--- src/oyna/ufo_explosion/grid_base.py
+++ src/oyna/ufo_explosion/grid_base.py
@@ -2,32 +2,11 @@ import random
 from time import sleep
 
 
-def getch() -> str:
-    """Gets a single character"""
-    try:
-        import msvcrt
-
-        return str(msvcrt.getch().decode("utf-8"))  # type: ignore
-    except ImportError:
-        import sys
-        import termios
-        import tty
-
-        fd = sys.stdin.fileno()
-        oldsettings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(fd)
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, oldsettings)
-        return ch
-
-
 class Board:
     def __init__(self, height: int) -> None:
-        self.height, self.length = height, height // 2 * 10
+        self.height, self.length = height // 2, height * 3
         self.cells: list[list[str]] = self._cells()
-        self.player = -2, 1
+        self.player = (-2, 1)
         self.cells[self.player[0]][self.player[1]] = "🛸"
         self.create_castle()
 
@@ -40,44 +19,33 @@ class Board:
             for i in range(self.height)
         ]
 
-    def action(self, character: str) -> None:
-        if character.isdigit():
-            self.move(int(character) * self.height // 2)
-        else:
-            exit()
-
     def move(self, step: int) -> None:
-        for i in range(step):
-            self.cells[self.player[0]][self.player[1]] = "  "
-
-            if i < self.height - 3 and i < step // 2:
-                self.player = self.player[0] - 1, self.player[1] + 1
-            elif i >= (step + 1) // 2 and step - i < self.height - 2:
-                self.player = self.player[0] + 1, self.player[1] + 1
-            else:
-                self.player = self.player[0], self.player[1] + 1
-            self.cells[self.player[0]][self.player[1]] = "🛸"
-            print(self)
-            sleep(0.05)
-        self._boom()
+        if step <= self.length - 4:
+            for i in range(step):
+                self.cells[self.player[0]][self.player[1]] = "  "
+                if i == 1:
+                    self.player = self.player[0] - 1, self.player[1] + 1
+                if i == step - 1:
+                    self.player = self.player[0] + 1, self.player[1] + 1
+                else:
+                    self.player = self.player[0], self.player[1] + 1
+                self.cells[self.player[0]][self.player[1]] = "🛸"
+                print(self)
+                sleep(0.05)
+            self._boom()
 
     def _boom(self) -> None:
-        for i in range((self.height + 1) // 2):
-            self.cells[self.player[0]][self.player[1] - i] = "💥"
+        self.cells[self.player[0]][self.player[1]] = "💥"
         print(self)
         sleep(0.3)
-        for i in range((self.height + 1) // 2):
-            self.cells[self.player[0]][self.player[1] - i] = "  "
+        self.cells[self.player[0]][self.player[1]] = "  "
         self.player = self.height - 2, 1
         self.cells[self.player[0]][self.player[1]] = "🛸"
         self.create_castle()
         print(self)
 
     def create_castle(self) -> None:
-        for cell in self.cells[-2]:
-            if cell == "🏠":
-                break
-        else:
+        if not any(cell == "🏠" for row in self.cells for cell in row):
             self.cells[self.height - 2][
                 random.randint(self.height // 2, self.length - 2)
             ] = "🏠"
@@ -89,11 +57,11 @@ class Board:
 
 
 def run() -> None:
-    board = Board(15)
+    board = Board(12)
     print(board)
 
     while True:
-        board.action(getch())
+        board.move(int(input(f"Insert steps count (1-{board.length - 4}): ")))
 
 
 if __name__ == "__main__":
